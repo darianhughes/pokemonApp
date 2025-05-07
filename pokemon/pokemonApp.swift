82c234bb2152efddb06d027efa9e1c4a1d91b07e
darianhughes <darianmhughes7@gmail.com>
2025-05-07 08:33:18 -0500

    Initial Commit

---
+++ pokemon/pokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokemonApp.swift
+//  pokemon
+//
+//  Created by HUGHES, DARIAN M. on 5/7/25.
+//
+
+import SwiftUI
+
+@main
+struct pokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
